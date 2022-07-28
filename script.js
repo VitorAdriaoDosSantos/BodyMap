c50962b70b21d83faaaac3ea967e86025272fef3
Vitor Adriao dos Santos <vitorsanto.3635@gmail.com>
2022-07-28 14:33:14 -0300

    first version BodyMap

---
+++ script.js
@@ -0,0 +1,6 @@
+const close = document.getElementById('close');
+const popup = document.getElementById('popup');
+
+close.addEventListener("click", function() {
+  popup.style.display = 'none';
+});
