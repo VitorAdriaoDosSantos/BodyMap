e6b5afb7cccbf67726532305fb9c6e1a925de606
Vitor Adriao dos Santos <vitorsanto.3635@gmail.com>
2023-01-07 17:52:51 -0300

    new file JS

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const close = document.getElementById('close');
-const popup = document.getElementById('popup');
+function popUp(){
 
-close.addEventListener("click", function() {
-  popup.style.display = 'none';
-});
+const body = document.body;
+
+const painel = document.createElement("div");
+painel.setAttribute("class","cssPainel");
+body.appendChild(painel);
+
+const msg = document.createElement("p");
+msg.textContent = "message";
+painel.appendChild(msg);
+
+const imgPopUp = document.createElement("img");
+imgPopUp.setAttribute("class","imgPopUp");
+imgPopUp.src = "PointOfImg.PNG";
+painel.appendChild(imgPopUp);
+
+const closeBtn = document.createElement("button");
+closeBtn.textContent= "X";
+painel.appendChild(closeBtn);
+
+closeBtn.addEventListener("click",()=> {painel.parentNode.removeChild(painel)} )
+}
